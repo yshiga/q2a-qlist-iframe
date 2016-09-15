e2336b98fb32c263e27f975363001e71c3f9aeb2
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-09-15 17:44:59 +0900

    Add plugin file

---
+++ qa-plugin.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+	Plugin Name: QList iframe Plugin
+	Plugin URI:
+	Plugin Description: Question List for iframe
+	Plugin Version: 1.0
+	Plugin Date: 2016-09-15
+	Plugin Author: 38qa.net
+	Plugin Author URI: http://38qa.net/
+	Plugin License: GPLv2
+	Plugin Minimum Question2Answer Version: 1.7
+	Plugin Update Check URI:
+*/
+
+if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
+	header('Location: ../../');
+	exit;
+}
+
+// page
+qa_register_plugin_module('page', 'qa-test-page.php', 'qa_test_page', 'Test Page');
